7264abeba310c6179c16d094e662c77cc1763e39
priii <get.priyab@gmail.com>
2017-04-07 19:57:55 -0700

    search workss

--- app.js
+++ app.js
@@ -22,21 +22,20 @@ var restaurants = [
     }
 ];
 
-// followed the below demo for search method
-// http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
+
 var viewModel = {
-  restaurants: ko.observableArray(restaurants),
-  query: ko.observable(''),
-  //search function
-  search: function(value) {
-    //remove all the current restaurants, which removes from the view
-    viewModel.restaurants.removeAll();
-    for(var x in restaurants) {
-      if(restaurants[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0){
-        viewModel.restaurants.push(restaurants[x]);
-      }
-    }
-  }
+  //restaurants: ko.observableArray(restaurants),
+  query: ko.observable('')
 };
-viewModel.query.subscribe(viewModel.search);
-ko.applyBindings(viewModel);
+  //search function
+  // followed the below demo for search method
+  // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+  // http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
+viewModel.restaurants = ko.computed(function(){
+  var search = this.query().toLowerCase();
+        return ko.utils.arrayFilter(restaurants, function(restaurant){
+        return restaurant.name.toLowerCase().indexOf(search)>=0;
+    });
+
+},viewModel);
+  ko.applyBindings(viewModel);
