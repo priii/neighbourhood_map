bb545079900d46ef1b2685866b3ce402363d1594
priii <get.priyab@gmail.com>
2017-04-24 18:13:55 -0700

    updated

--- mappy.js
+++ mappy.js
@@ -74,7 +74,7 @@ function populateInfoWindow(marker, infowindow) {
   // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.setContent('<div>' + marker.name + '</div>');
     infowindow.open(map, marker);
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick',function(){
