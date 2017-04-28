d37ee84b8b1226007dec4a61f87c76e2d575b428
priii <get.priyab@gmail.com>
2017-04-28 00:29:28 -0700

    foursquare works

--- mappy.js
+++ mappy.js
@@ -3,41 +3,48 @@ var map;
 var markers =[];
 var currentInfoWindow;
 
-// restaurant details
-
+// Array that contains restaurant details
+// venue id taken from fourSquare
 var restaurants = [
   { name: "Orean's Hummus",
     caption: ": Cozy Mediterranean restaurant",
+    venue_id: "53c2fe37498ef47b8dd63ab1",
     location: {lat: 37.3948, lng: -122.0787}
   },
   {
     name:"Taqueria La Espuela",
     caption:" : Mexican staples",
+    venue_id:"49ee3169f964a52036681fe3",
      location: {lat: 37.3929, lng: -122.0803}
   },
  {
    name: "Niji Sushi",
    caption:": Informal, bustling Japanese eatery",
+   venue_id:"501acdfbe4b0981c98c5a8dc",
    location: {lat: 37.3922, lng: -122.0792}
  },
  {
    name:"Crepevine",
    caption:": sweet & savory crêpes.",
+   venue_id: "50269a95e4b0727083f082b1",
    location: {lat: 37.3925, lng: -122.0800}
  },
  {
    name:"Xanh Restaurant",
    caption:": High-end Vietnamese dishes",
+   venue_id: "4a693c9af964a520d1cb1fe3",
    location: {lat: 37.394895, lng: -122.078572}
     }
 ];
 
 
 
-// // this code is taken from  Google Maps API Course on Udacity
-
+ // this code is taken from  Google Maps API Course on Udacity
+// initializing the map
 function initMap(){
+
   // Create a styles array to use with the map.
+
   var styles = [
     {
       featureType: 'water',
@@ -104,7 +111,9 @@ function initMap(){
       ]
     }
   ];
+
   //Constructor to create a new map.
+
   map = new google.maps.Map(document.getElementById('map'),{
     center: {lat: 37.386051, lng: -122.083855},
     zoom: 13,
@@ -116,7 +125,6 @@ function initMap(){
 
   // Style the markers a bit. This will be our listing marker icon.
       var defaultIcon = makeMarkerIcon('0091ff');
-
       // Create a "highlighted location" marker color for when the user
       // mouses over the marker.
       var highlightedIcon = makeMarkerIcon('FFFF24');
@@ -137,12 +145,10 @@ function initMap(){
       id: i
     });
     var largeInfowindow = new google.maps.InfoWindow();
-
-  var bounds = new google.maps.LatLngBounds();
-
-  restaurants[i].marker = marker;
-  // push the marker
-  markers.push(marker);
+    var bounds = new google.maps.LatLngBounds();
+    restaurants[i].marker = marker;
+    // push the marker
+    markers.push(marker);
 
   marker.addListener('click', function(){
     populateInfoWindow(this, largeInfowindow);
@@ -165,8 +171,6 @@ setTimeout(function(){
 },150);
 }
 
-
-
 // This function populates the infowindow when the marker is clicked. We'll only allow
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
@@ -174,12 +178,13 @@ function populateInfoWindow(marker, infowindow) {
   // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.name + '</div>');
+  //  infowindow.setContent('<div>' + marker.name + '</div>');
     infowindow.open(map, marker);
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick',function(){
       infowindow.setMarker = null;
     });
+    
     var streetViewService = new google.maps.StreetViewService();
             var radius = 50;
             // In case the status is OK, which means the pano was found, compute the
@@ -190,16 +195,46 @@ function populateInfoWindow(marker, infowindow) {
                 var nearStreetViewLocation = data.location.latLng;
                 var heading = google.maps.geometry.spherical.computeHeading(
                   nearStreetViewLocation, marker.position);
-                  infowindow.setContent('<div>' + marker.name + '</div><div id="pano"></div>');
-                  var panoramaOptions = {
-                    position: nearStreetViewLocation,
-                    pov: {
-                      heading: heading,
-                      pitch: 30
+                  // finding venue_id for the restaurants
+                  var rest_idx = 0;
+                    for(var i=0;i< restaurants.length; i++){
+                      if( marker.name === restaurants[i].name){// if the clicked marker's name and restaurants name matches ! return its index
+                         rest_idx = i;
+                      }
                     }
-                  };
-                var panorama = new google.maps.StreetViewPanorama(
-                  document.getElementById('pano'), panoramaOptions);
+
+                  venue_id = restaurants[rest_idx].venue_id; // passing the index value
+                  // FourSquare api request
+                  // getting 3 tips texts from the foursquare api
+                var fourSquareurl ='https://api.foursquare.com/v2/venues/'+ venue_id +'/tips?sort=recent&client_id=CI2GYOIPPOSMQEIJ4VT20VCKTBEJ3M23APK1CZ0C335YCQ4G&client_secret=IUIE4BVBKECRGK3UYMRUNQ5P0MJ2F2GXHYZ3PPDYIZGFD4FV&v=20170426';
+                       $.ajax({
+                url: fourSquareurl,
+                dataType: 'json',
+                data:{
+                        async: true
+                },
+                success: function(data){
+                  var tip1 = data.response.tips.items[0].text;
+                  var tip2 = data.response.tips.items[1].text;
+                  var tip3 = data.response.tips.items[2].text;
+                  infowindow.setContent('<div>' + marker.name+ '</div><hr><div id="pano"></div><br>'+ '<div><br><h3>'+'3 customer reviews about this restaurant'+'</h3></div>'+tip1 + '<div><br>'+ tip2 +'</div><br>'+ '<div>'+ tip3 +'</div>');
+                   var panoramaOptions = {
+                     position: nearStreetViewLocation,
+                     pov: {
+                       heading: heading,
+                       pitch: 30
+                     }
+                   };
+                 var panorama = new google.maps.StreetViewPanorama(
+                   document.getElementById('pano'), panoramaOptions);
+                },
+                error: function(data) {
+                      alert("Could not load data from foursquare.");
+                }
+              });
+
+                  //fourSquareInfo= return_val;
+
               } else {
                 infowindow.setContent('<div>' + marker.name + '</div>' +
                   '<div>No Street View Found</div>');
@@ -245,7 +280,7 @@ function vm() {
   self.clicked = ko.computed(function(){
        var query = self.query().toLowerCase();
        return ko.utils.arrayFilter(self.locations(),function(temp_var){
-        var store = temp_var.name.toLowerCase().indexOf(query)>=0; // checks the each index of query's value and temp_var' value if bot matches it will return 0 if not it will return -1(false) nothing will show up!
+        var store = temp_var.name.toLowerCase().indexOf(query)>=0; // checks the each index of query's value and temp_var' value if both matches it will return 0 if not it will return -1(false) nothing will show up!
       //  var marker = temp_var.marker;
         if(temp_var.marker){
           temp_var.marker.setVisible(true);
@@ -257,8 +292,9 @@ function vm() {
             temp_var.marker.setVisible(true);
         } else {
            temp_var.marker.setVisible(false);
-            console.log(temp_var.marker);
+
         }
+
       });
   });
 
